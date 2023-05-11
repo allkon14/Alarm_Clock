bad3a62f5affa84b2f799fd186463a8706299a2b
allkon14 <alena.konoplevaa@yandex.ru>
2023-05-11 14:13:21 +0400

    add time picker and buttons

---
+++ app/src/main/java/com/example/alarmclock/AlarmReceiver.kt
@@ -0,0 +1,19 @@
+package com.example.alarmclock
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver :BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        var getResult: String? = intent!!.getStringExtra("extra")
+
+        var service_intent: Intent = Intent(context, RingtoneService::class.java)
+        service_intent.putExtra("extra", getResult)
+        context!!.startService(service_intent)
+
+    }
+
+}
